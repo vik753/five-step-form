6627929d92bf62b595bf6aa4bbe0b76ef11a9928
IhorKorenets <vik753@gmail.com>
2026-09-01 17:21:17 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import {RegistrationForm} from "@/pages/Registration/RegistrationForm.jsx";
+
+
+function App() {
+
+
+	return (
+			<RegistrationForm/>
+	);
+}
+
+export default App;
---
+++ src/pages/Registration/RegistrationForm.jsx
@@ -0,0 +1,9 @@
+import "../../styles/registration.css"
+
+export const RegistrationForm = () => {
+
+
+	return (
+			<h1>Registration Form</h1>
+	);
+};
